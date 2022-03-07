821b35141afec3585bb370e85e85f03e3234906d
Rob <macharborguy@gmail.com>
2022-03-07 16:05:12 -0500

    add more vue router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
-<template lang="pug">
-v-app
-	v-main
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-export default {
-	name: 'App',
-	components: {},
-	data: ()=>({}),
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
+nav {
+  padding: 30px;
 
-<style lang="stylus">
-body
-	background-color red
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
